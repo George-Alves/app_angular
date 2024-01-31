49744cb435517da72bf189254690be28b64b5882
George Alves <115368476+George-Alves@users.noreply.github.com>
2024-01-30 23:37:48 -0300

    [WIP] Ajuste do Front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidenavComponent } from "./sidenav/sidenav.component";
+import { CommonModule } from '@angular/common';
+import { BodyComponent } from "./body/body.component";
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapesed: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, SidenavComponent, RouterModule, CommonModule, BodyComponent]
 })
 export class AppComponent {
   title = 'app_angular';
+
+  isSideNavCollapesed = false;
+  screenWidth = 0;
+
+  onToggleSideNav(data: SideNavToggle): void{
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapesed = data.collapesed;
+
+  }
 }
